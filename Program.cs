997c3ba52428469bd7869cdb4cf687b08bee4fba
icc-antonenko <megggakill@gmail.com>
2017-07-11 15:14:45 +0500

    sdfsdfsdf

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace _1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("!!!");
+            Console.WriteLine("!!!?");
         }
     }
 }
